0392be141afba0be50cd679cf15fab6673aa73a1
Ville <villehikaikkonen@gmail.com>
2020-04-08 12:13:45 +0300

    Bug fix

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                     $result = mysqli_query($connection, $sql); // Uses open.php to send SQL query
                     if (mysqli_num_rows($result) > 0) { // Checks if the query provided any results
                         while ($row = mysqli_fetch_assoc($result)) { // Displays results for every one found in database
-                            echo '<tr><td><a href="/article.php?id=' . $row["ID"] . '"<h3>' . $row["Headline"] . '</h3><p></p></td></a></tr>';
+                            echo '<tr><td><a href="/article.php?id=' . $row["ID"] . '"><h3>' . $row["Headline"] . '</h3><p></p></td></a></tr>';
                         }
                     } else {
                         echo "No results";
