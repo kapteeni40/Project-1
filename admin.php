f91ec15024cdbb0eccc2291ab979bf4e5beca241
Aatu Yli-Hukkala <ylihukkalaa@gmail.com>
2020-04-29 17:10:23 +0300

    Styles for the admin page

--- admin.php
+++ admin.php
@@ -47,19 +47,23 @@ if (!isset($_SESSION['userID'])) {
             include 'config.php'; // Connection configuration
             include 'open.php'; // Database connection
         ?>
-        <a href='/logout.php'>Log Out</a>
-        <a href='/article_edit.php'><h3>New Article</h3></a>
-        <?php 
-            $sql = "SELECT ID, Headline FROM Articles";
-            $result = mysqli_query($connection, $sql); // Uses open.php to send SQL query
-            if (mysqli_num_rows($result) > 0) { // Checks if the query provided any results
-                while ($row = mysqli_fetch_assoc($result)) { // Displays results for every one found in database
-                    echo '<tr><td><a href="/article_edit.php?id=' . $row["ID"] . '"><h3>' . $row["Headline"] . '</h3><p></p></a></td></tr>';
+        <button type="button"><a href='/logout.php'>Log Out</a></button>
+        <div class="container">
+            <table>
+            <?php 
+                $sql = "SELECT ID, Headline FROM Articles";
+                $result = mysqli_query($connection, $sql); // Uses open.php to send SQL query
+                if (mysqli_num_rows($result) > 0) { // Checks if the query provided any results
+                    while ($row = mysqli_fetch_assoc($result)) { // Displays results for every one found in database
+                        echo '<tr><td><h3>' . $row["Headline"] . '</h3></td><td><a href="/article_edit.php?id=' . $row["ID"] . '"><h3>Edit article</h3></a></td></tr>';
+                    }
+                } else {
+                    echo "No results";
                 }
-            } else {
-                echo "No results";
-            }
-            include 'close.php'; // Close connection
-        ?>
+                include 'close.php'; // Close connection
+            ?>
+            <tr><td></td><td><a href='/article_edit.php'><h3>New Article</h3></a></td></tr>
+            </table>
+        </div>
     </body>
 </html>
